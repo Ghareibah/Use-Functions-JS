db9335aea5066d41536ae7357073b7d7bdd21e37
GhareibahAlyammahi <gh1graphics@gmail.com>
2022-12-05 22:23:29 +0400

    first commit

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "salad 1",
+    price: 12.5,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/01.png",
+  },
+  {
+    id: 1,
+    name: "salad 2",
+    price: 13.5,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/02.png",
+  },
+  {
+    id: 2,
+    name: "salad 3",
+    price: 12.5,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/03.png",
+  },
+  {
+    id: 3,
+    name: "salad 4",
+    price: 13.5,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/04.png",
+  },
+  {
+    id: 4,
+    name: "salad 5",
+    price: 13.5,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/05.png",
+  },
+  {
+    id: 5,
+    name: "salad 6",
+    price: 13.5,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/06.png",
+  },
+];
